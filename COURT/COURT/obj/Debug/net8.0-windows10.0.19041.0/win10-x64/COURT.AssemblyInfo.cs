695caf30f244a1b2f9778fdd0691bd22b7e70d74
Maël COURT <maelcourt0@gmail.com>
2024-04-14 20:35:01 +0200

    changement d'icone

--- COURT/COURT/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/COURT.AssemblyInfo.cs
+++ COURT/COURT/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/COURT.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COURT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fe56bd2fd842d4d9c788a4e5404f56b7fe26c8f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3c0d64e6b9e5459c42f2f5653ffeac54fce159e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("COURT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COURT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
